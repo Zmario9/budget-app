44a5f273e37f2ba60070d6a849f74c452050b8cc
Jose Manuel <jcorreacastro99@gmail.com>
2024-09-25 21:30:31 -0400

    First commit creating basic components for the current project

---
+++ src/app/components/budget-view/budget-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-budget-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './budget-view.component.html',
+  styleUrl: './budget-view.component.scss'
+})
+export class BudgetViewComponent {
+
+}
---
+++ src/app/components/table-chart/table-chart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-chart',
+  standalone: true,
+  imports: [],
+  templateUrl: './table-chart.component.html',
+  styleUrl: './table-chart.component.scss'
+})
+export class TableChartComponent {
+
+}
